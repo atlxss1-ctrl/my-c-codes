1948c49625e287a6ce6769d1c47f91e589008b37
Beytullah Aksoy <atlxss1@gmail.com>
2025-12-28 21:59:02 +0300

    Comment out Windows mouse tracking program

--- windows.h
+++ windows.h
@@ -1,1 +1,37 @@
+/*#include <windows.h>  // Windows'un beynine giden kapi
 
+int main() {
+   
+    
+    MessageBox(NULL, "Bu mesaj ATLXSSQ1! tarafindan gonderildi!\nSisteme erisim basarili.", "** Hacked **", MB_OK | MB_ICONWARNING);
+    
+    return 0;
+}
+*/
+-------------------------------------------------------------------------------------------------------------------------------------
+/*
+#include <windows.h> 
+#include <stdio.h>   
+
+int main() {
+    
+    POINT p; 
+
+    printf("Mouse Takip Programi Baslatildi...\n");
+    printf("Cikmak icin bu pencereyi kapatabilirsin.\n\n");
+
+
+    while (1) {
+       
+        GetCursorPos(&p);
+
+       
+        printf("\rX Koordinati: %ld  |  Y Koordinati: %ld   ", p.x, p.y);
+
+        
+        Sleep(50);
+    }
+
+    return 0;
+}
+*/
